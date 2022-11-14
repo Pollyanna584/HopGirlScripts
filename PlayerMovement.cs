7fc769d7924fe023067d689866c2b82fdde841d4
Pollyanna584 <monk.phillip@gmail.com>
2022-11-13 19:45:38 -0800

    created PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //create variable reference
+    // e.g. public <type> <name>:
+    public Rigidbody2D body;
+    //[SerializeField] to create variable change within Unity
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpHeight;
+
+    //Awake only runs when script is called
+    private void Awake(){
+        //Fine RigidBody2d component of object where script is, assign properties to the variable
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // Update checks every frame, good for movement
+    private void Update(){
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        if(Input.GetAxis("Jump"));
+            body.velocity = new Vector2(body.velocity.x, Input.GetAxis("Jump") * jumpHeight);
     }
 }
