0a827e41435c27ad05418fd82438354ff74307c7
Pollyanna584 <monk.phillip@gmail.com>
2022-11-14 17:28:18 -0800

    player movement and wall jump working.  created animation for attack and started fireball script.

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
     //[SerializeField] to create variable change within Unity
     [SerializeField] private float speed;
     [SerializeField] private float jumpHeight;
+    [SerializeField] private float wallPushback;
+    [SerializeField] private float wallJumpHeight;
 
     //create variable reference
     // e.g. public <type> <name>:
@@ -15,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
     private float wallJumpCooldown;
+    private float horizontalInput;
 
     //Awake only runs when script is called
     private void Awake(){
@@ -27,7 +30,7 @@ public class PlayerMovement : MonoBehaviour
     // Update checks every frame, good for movement
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
 
 
         //will check if key is pressed constantly   
@@ -49,30 +52,46 @@ public class PlayerMovement : MonoBehaviour
         anim.SetBool("grounded", isGrounded());
 
         //wall jump logic
-        if(wallJumpCooldown < 0.2f){
-            if(Input.GetKey(KeyCode.Space) && isGrounded())
-                Jump();
+        if(wallJumpCooldown > 0.2f){
+
             body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-            if (onWall() && isGrounded()){
+            if (onWall() && !isGrounded()){
                 body.gravityScale = 0;
                 body.velocity = Vector2.zero;
             }
         else
             body.gravityScale = 3;
 
+        if(Input.GetKey(KeyCode.Space))
+            Jump();
         }
+        else
+            wallJumpCooldown += Time.deltaTime;
     }
 
     private void Jump(){
-        body.velocity = new Vector2(body.velocity.x, jumpHeight);
-        anim.SetTrigger("jump");
+        if(isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpHeight);
+            anim.SetTrigger("jump");
+        }
+        else if(onWall() && !isGrounded())
+        {
+            if(horizontalInput == 0)
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 3);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * wallPushback, wallJumpHeight);
+            }
+            wallJumpCooldown = 0;
 
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision){
-
-    }
 
     private bool isGrounded(){
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
@@ -82,5 +101,10 @@ public class PlayerMovement : MonoBehaviour
     private bool onWall(){
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
         return raycastHit.collider != null;
+    }
+    
+    public bool canAttack(){    
+        //tk make able to attack while moving
+        return horizontalInput == 0 && isGrounded() && !onWall();
     }
 }
