66df40a498f0df66aea786087727ce48b070296b
Pollyanna584 <monk.phillip@gmail.com>
2022-11-15 21:41:27 -0800

    This version has the player followed by the camera.  Revert to last build or check comments under public void Update to change to room to room camera.

--- CameraController.cs
+++ CameraController.cs
@@ -2,13 +2,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+    //room camera
     [SerializeField] private float speed;
     private float currentPosX;
     private  Vector3 velocity = Vector3.zero;
 
-    public void Update(){
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+    //follow player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
 
+    public void Update(){
+
+        //Room camera
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        //follow player
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
         }
     
     public void MoveToNewRoom(Transform _newRoom){
